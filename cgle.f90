9ff8fe2431e1ae659fb2d8d02a273852ac97c8a5
Connor Glosser <glosser1@gmail.com>
2014-04-16 17:07:21 -0400

    Better variables, simpler functions

--- cgle.f90
+++ cgle.f90
@@ -25,21 +25,20 @@ module D2Q7Const
   double precision, parameter :: soundSpeed = 2d0
   integer, parameter :: reverse(0:numQ - 1) = (/0, 4, 5, 6, 1, 2, 3/)
   integer, parameter :: latticeDim = 2
-  
 end module D2Q7Const
 
 module simParam
-  !for the love of god, make sure rDim is even for periodic D2Q7!
+  ! Make sure cDim is even for periodic D2Q7!
   integer, parameter                 :: rDim   = 231
   integer, parameter                 :: cDim   = 200
-  integer, parameter                 :: tMax   = 1000
-  double precision, parameter        :: deltaX = 0.1d0, deltaT = 0.05d0   ! dT = knudsen #
+  integer, parameter                 :: tMax   = 2400
+  double precision, parameter        :: boxLength = 10d0
+  double precision, parameter        :: deltaX = boxLength/rDim, deltaT = 0.05d0   ! dT = knudsen #
   double precision, parameter        :: tau    = 0.55d0
   double precision, parameter        :: t0_coef= 0.3d0
-  double precision, parameter        :: boxLength = 10d0
   complex(kind=kind(0d0)), parameter :: lambda = 2d0/(deltaT*(2*tau - 1))
   complex(kind=kind(0d0)), parameter :: beta   = 2.0d-3                   ! beta = D0
-  complex(kind=kind(0d0)), parameter :: a = dcmplx(0.100d0, 0.24d0)
+  complex(kind=kind(0d0)), parameter :: a = dcmplx(0.100d0, 0.00d0)
   complex(kind=kind(0d0)), parameter :: d = dcmplx(0.025d0, 0.03d0)
 end module simParam
 
@@ -49,7 +48,7 @@ program cgle
   implicit none
 
   complex(kind=kind(0d0)), allocatable :: f(:, :, :), feq(:, :, :), rho(:, :), omega(:, :)
-  double precision :: sumF = 0d0
+  double precision :: f_rsq = 0d0
   integer :: time, r, c
 
   allocate(f(rDim, cDim,0:numQ - 1), feq(rDim, cDim,0:numQ - 1))
@@ -58,18 +57,18 @@ program cgle
   open(unit=20,file="realpart.dat")
   open(unit=30,file="imagpart.dat")
 
-  call setInitialF(f)
-  call computeMacros(f, rho, omega)
+  !call initRandomF(f)
+  call initSpiralF(f)
   do time = 1, tMax
-    write(*,*) time, sumF
+    write(*,*) time, f_rsq
     call computeMacros(f, rho, omega)
     call computeFeq(rho, feq)
     call collide(feq, omega, f)
     call stream(f)
 
-    sumF = sum(real(f)**2 + aimag(f)**2)
-    f = f/(sqrt(sumF/(rDim*cDim*numQ)))
-    if(mod(time, 500) .eq. 0) then
+    f_rsq = sum(real(f)**2 + aimag(f)**2)
+    f = f/sqrt(f_rsq/size(f))
+    if(mod(time, 40) .eq. 0) then
       do c = 1, cDim
         write(20,*) real(rho(:,c))
         write(30,*) aimag(rho(:,c))
@@ -109,7 +108,7 @@ subroutine computeFeq(rho, feq)
 
   feq(:, :,0) = (1 - lambda*beta*latticeDim/soundSpeed**2)*rho(:, :)
 
-  tmp(:, :) = lambda*beta*latticeDim/(numQ*soundSpeed**2)*rho(:, :)
+  tmp(:, :) = lambda*beta*latticeDim/((numQ - 1)*soundSpeed**2)*rho(:, :)
   do dir = 1, numQ - 1
     feq(:, :, dir) = tmp(:, :)
   end do
@@ -183,13 +182,14 @@ subroutine collide(fEq, omega, f)
     f(:,:,dir) = f(:,:,dir) - 1/tau*(f(:,:,dir) - fEq(:,:,dir)) + omega
   end do
 
+  ! Neumann boundary conditions
   f(:,1,:)     = f(:,2,:)
   f(:, cDim,:) = f(:, cDim - 1,:)
   f(1,:,:)     = f(2,:,:)
   f(rDim,:,:)  = f(rDim - 1, :,:)
 end subroutine collide
 
-subroutine setInitialF(f)
+subroutine initSpiralF(f)
   use D2Q7Const, only: numQ
   use simParam,  only: rDim, cDim, t0_coef, deltaX, boxLength
   implicit none
@@ -198,10 +198,27 @@ subroutine setInitialF(f)
   integer :: rIdx, cIdx
   double precision :: x, y
   do cIdx = 1, cDim
-    x = cIdx*deltaX - boxLength
+    x = cIdx*deltaX - boxLength/2
     do rIdx = 1, rDim
-      y = rIdx*deltaX - boxLength
+      y = rIdx*deltaX - boxLength/2
       f(rIdx, cIdx, :) = t0_coef*dcmplx(x, y)/numQ
     end do
   end do
-end subroutine setInitialF
+end subroutine initSpiralF
+
+subroutine initRandomF(f)
+  use D2Q7Const, only: numQ
+  use simParam,  only: rDim, cDim, t0_coef, deltaX, boxLength
+  implicit none
+  
+  complex(kind=kind(0d0)), intent(out) :: f(rDim, cDim, 0:numQ - 1)
+  integer :: rIdx, cIdx
+  double precision :: rands(2)
+  do cIdx = 1, cDim
+    do rIdx = 1, rDim
+      call random_number(rands)
+      rands = rands - 0.5d0
+      f(rIdx, cIdx, :) = t0_coef*dcmplx(rands(1), rands(2))/numQ
+    end do
+  end do
+end subroutine initRandomF
