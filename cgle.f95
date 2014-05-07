4efc667f6d0aab5ab08780864e5da4cd6450637f
Connor Glosser <glosser1@gmail.com>
2014-05-07 14:47:36 +0200

    Fixed unused variable warning

--- cgle.f95
+++ cgle.f95
@@ -5,7 +5,7 @@ program cgle
   use CGLEvis
   implicit none
 
-  integer :: time, c
+  integer :: time
   real(kind=real64) :: f_rsq = 0d0
   complex(kind=real64), allocatable :: f_density(:, :, :), feq(:, :, :)
   complex(kind=real64), allocatable :: rho(:, :), omega(:, :)
